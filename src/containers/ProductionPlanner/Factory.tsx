bc8f98010f68717e5c7730393a56c6d3ab144f12
Rane <rane.fields@gmail.com>
2021-11-28 04:02:00 -0800

    Remove unused fonts

--- src/containers/ProductionPlanner/Factory.tsx
+++ src/containers/ProductionPlanner/Factory.tsx
@@ -15,14 +15,14 @@ const Factory = () => {
     <>
       <Card style={{ marginBottom: '25px' }}>
         <Title order={2}>
-          Welcome back, &lt;Engineer ID #{ctx.engineerId}&gt;,
+          Welcome back &lt;Engineer ID #{ctx.engineerId}&gt;,
         </Title>
         <Text>
           This tool has been created to increase the efficiency of your work towards Project Assembly.<br />
           We hope that you will continue to be effective.
         </Text>
         <Divider style={{ marginTop: '10px', marginBottom: '10px' }} />
-        <Text style={{ fontSize: '12px' }}>
+        <Text style={{ fontSize: '13px' }}>
           {ctx.ficsitTip}
         </Text>
       </Card>
